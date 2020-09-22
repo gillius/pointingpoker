66a123e56eca42e6101865f75f47de8401d48a2b
Jason Winnebeck <gillius@gillius.org>
2020-09-21 23:40:06 -0400

    Simplify DebuggingComponent.js with React hooks

--- client/src/DebuggingComponent.js
+++ client/src/DebuggingComponent.js
@@ -1,50 +1,33 @@
-import React, {Component} from "react";
+import React, {useState} from "react";
 import PropTypes from 'prop-types';
 
-export default class DebuggingComponent extends Component {
-	static propTypes = {
-		pendingActions: PropTypes.array,
-		messages: PropTypes.array,
-	}
+export default function DebuggingComponent({pendingActions, messages}) {
+	const [showDebugging, setShowDebugging] = useState(false);
 
-	constructor(props, context) {
-		super(props, context);
-
-		this.state = {
-			showDebugging: false
-		}
-	}
-
-	toggleShow = () => {
-		this.setState(s => ({showDebugging: !s.showDebugging}));
-	}
-
-	render() {
-		let messages;
-		if (this.state.showDebugging) {
-			messages =
-					<div>
-						<p>Pending Actions:</p>
-						{
-							this.props.pendingActions.map(it => (
-									<p key={it.seq}>{JSON.stringify(it)}</p>
-							))
-						}
-						<p>Messages:</p>
-						{
-							this.props.messages.map((it, index) => (
-									<p key={index}>{it}</p>
-							))
-						}
-					</div>
-		}
-
-		return (
+	return (
+			<>
+				<p><button onClick={() => setShowDebugging(!showDebugging)}>
+					{showDebugging ? 'Hide' : 'Show'} Debugging Messages</button></p>
+				{showDebugging &&
 				<>
-					<p><button onClick={this.toggleShow}>
-						{this.state.showDebugging ? 'Hide' : 'Show'} Debugging Messages</button></p>
-					{messages}
+					<p>Pending Actions:</p>
+					{
+						pendingActions.map(it => (
+								<p key={it.seq}>{JSON.stringify(it)}</p>
+						))
+					}
+					<p>Messages:</p>
+					{
+						messages.map((it, index) => (
+								<p key={index}>{it}</p>
+						))
+					}
 				</>
-		);
-	}
+				}
+			</>
+	);
 }
+DebuggingComponent.propTypes = {
+	pendingActions: PropTypes.array,
+	messages: PropTypes.array,
+};
